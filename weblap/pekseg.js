40505b15f0af3379374fb8071188308eca082446
19cadenemba <19cadenemba@derimiksa.hu>
2020-06-05 16:56:02 +0200

    a weblap első változata, hiányzik még a tartalom, és nem reszponzív a formázás

---
+++ weblap/pekseg.js
@@ -0,0 +1,30 @@
+console.warn('Hello');
+
+var fo_resz = document.getElementById('fo');
+
+function csere_001()
+{
+    var x = document.getElementById('001').innerHTML;
+    document.getElementById('fo').innerHTML = x;
+}
+
+function csere_002()
+{
+    var x = document.getElementById('002').innerHTML;
+    document.getElementById('fo').innerHTML = x;
+}
+
+function csere_001()
+{
+    var x = document.getElementById('003').innerHTML;
+    document.getElementById('fo').innerHTML = x;
+}
+
+
+
+
+function katt()
+                {
+                    $(document.getElementById('m2')).removeClass("intro").addClass("main");
+                });
+        });
